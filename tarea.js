f0fd1d11022edcbdd3201c8f78c99a090c943dd2
gerald <mbsistemsgrafics@gmail.com>
2023-08-04 00:02:27 -0400

    tarea de gestion

---
+++ tarea.js
@@ -0,0 +1,23 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero1<numero2){
+    console.log("El numero 1 no es mayor que el numero 2");
+}else{
+    console("Vuelve a intentar");
+}
+if(numero2>0){
+    console.log("El numero 2 es positivo");
+}else{
+    console("Vuelve a intentar");
+}
+if(numero1>!0){
+    console.log("numero 1 es negativo o distinto de cero");
+}else{
+    console("Vuelve a intentar");
+}
+if((numero1=numero1+1) < numero2){
+    console.log("Incrementar en 1 unidad el valor de numero 1 no lo hace mayor o igual que numero 2");
+}else{
+    console("Vuelve a intentar");
+}
