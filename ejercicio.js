47845aeacd27f3ab6f4fea9175bff5e0f6ef42c7
oanjabul <oanjabul@gmail.com>
2023-08-03 23:31:53 -0400

    intento de subir el ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,14 @@
+var num1 = 5;
+var num2 = 8;
+if(num1 < num2){
+    alert("número1 no es mayor que número2");
+}
+if(num2 > 0){
+    alert("número2 es positivo");
+}
+if(num2 < 0 || num2 !=0){
+    alert("número1 no es negativo o distinto de cero");
+}
+if(num1 += 1 >= num2){
+    alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que número2");
+}
