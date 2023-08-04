5d2752a8df085ab64b752f96e2e954cc0c037619
Jhimy Fuentes Rojas <fuentesrojasjhimy@gmail.com>
2023-08-03 21:37:33 -0400

    Exmenen, ejercicios js

---
+++ examen.js
@@ -0,0 +1,15 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero1<numero2){
+    alert("numero1 no es mayor que el numero2");
+}
+if(numero2>0){
+    alert("el numero 2 es positivo");
+}
+if(numero1>!0){
+    alert("numero1 es negativo distinto que cero");
+}
+if((numero1=numero1+1) < numero2){
+    alert("Ingrementar en 1 unidad el valor del numero1 no le hace mayor o igual que numero2");
+}
