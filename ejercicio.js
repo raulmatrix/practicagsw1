fc278ced1882998a48d740c23932fd0251911d50
= <=>
2023-08-03 21:46:31 -0400

    Practica GitHub

---
+++ ejercicio.js
@@ -0,0 +1,14 @@
+var numero1 = 5;
+var numero2 = 8;
+if(numero1 < numero2){
+    alert("número1 no es mayor que número2");
+}
+if(numero2 > 0){
+    alert("número2 es positivo");
+}
+if(numero2 < 0 || numero2 !=0){
+    alert("número1 no es negativo o distinto de cero");
+}
+if(numero1 += 1 >= numero2){
+    alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que número2");
+}
