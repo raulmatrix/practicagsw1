60cf395b555b3ae9aeb88bd2570cdc6099af75c5
elmer <emamani1996@gmail.com>
2023-08-03 21:34:52 -0400

    Practica de js Gestions de sofware

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # practicagsw1
 Practica 1 tercer bimestre
+Nombres
+Cristian Alberto Condori Machaca
+Elmer Mamani
+
---
+++ tareaGstion.js
@@ -0,0 +1,15 @@
+var numero1=5;
+var numero2=8;
+
+If(numero1<numero2){
+	alert("Numero1 no es mayor que numero 2");
+}
+If(0<numero2){
+	alert("Numero2 es positivo");
+}
+If(numero1<0){
+	alert("Numero1 es negativo o distivo a cero");
+}
+If(numero1<numero2){
+	alert("Incrementar en 1 ");
+}
