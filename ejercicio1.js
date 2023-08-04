ec460670b393f65e33d1f90c1dece874d92abe02
Raven-312 <bryanhk18@gmail.com>
2023-08-03 21:32:17 -0400

    ejercicio terminado

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if (numero1 < numero2)
+{
+    alert("número 1 no es mayor que número 2");
+}
+
+if (numero2 > 0) 
+{
+    alert("número 2 es positivo");
+}
+
+if (numero1 < 0 || numero1 !== 0)
+ {
+    alert("número 1 es negativo o distinto de cero");
+}
+
+if (numero1 +1 <= numero2) 
+{
+    alert("incrementar en 1 unidad el valor de número 1 no lo hace mayor o igual que número 2");
+}
