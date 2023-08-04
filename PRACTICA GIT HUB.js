3d6253b602b0014d730b11a7e1f4a8cbc81f849a
Raul <Condori.Raul.800@gmail.com>
2023-08-03 21:47:33 -0400

    practicaGitHub

---
+++ PRACTICA GIT HUB.js
@@ -0,0 +1,17 @@
+
+//Dado el siguiente código, completar las condiciones de los IF
+
+var numerol = 5;
+var numer02 = 8;
+if(numerol <= numer02) {
+    alert("numerol no es mayor que numer02");
+}
+if(numer02 > 0) {
+    alert("numer02 es positivo");
+}
+if(numerol != 0) {
+    alert("numerol es negativo o distinto de cero");
+}
+if(numerol + 1 < numer02) {
+    alert("lncrementar en 1 unidad el valor de numerol no lo hace mayor o igual que numero2");
+}
